1de0977168c5b17bd9fe33516b25a6b81dceb494
Teodor-DSU <teodor.schulman@gmail.com>
2023-11-11 00:21:57 +0100

    Firetwerks!
    Healty even, strait from the... whatever pumpkins are

---
+++ GameJam_Unity_Folder/Assets/Scripts/Teo/ActivateParticleOnVoidEvent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateParticleOnVoidEvent : MonoBehaviour
+{
+    [SerializeField] private VoidEventSO voidEvent;
+    [SerializeField] private List<ParticleSystem> particleSystem;
+
+    private void OnEnable() 
+    {
+        voidEvent.OnEventRaised += PlayParticles;
+    }
+
+    private void OnDisable()
+    {
+        voidEvent.OnEventRaised -= PlayParticles;
+    }
+
+    private void PlayParticles()
+    {
+        for(int i = 0; i < particleSystem.Count; i++)
+        {
+            particleSystem[i].Play();
+        }
+    }
+}
